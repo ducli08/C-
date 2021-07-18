2d07ec89951b724fbf7d9f22c6f1c35a1fa8e096
ducli08 <leminhduc8821@gmail.com>
2021-07-18 16:31:28 +0700

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(n);
+            List<int> name = new List<int>(n);
+           
+            for(int i=0;i<n;i++)
+            {
+                name.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            for(int i=0;i<n;i++)
+            {
+                Console.Write(name[i]+" ");
+            }    
+        }
+    }
+}
